4d929635f5791d8551546a17e35766faeaa94644
swanchip <93869976+rouias11@users.noreply.github.com>
2026-07-11 15:07:24 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    confessionChannel: "1523425911469506764",
+
+    modAlertChannel: "1510278929666146317",
+
+    lowMediumPing: [
+        "1510273298850910348",
+        "1510273221172396032"
+    ],
+
+    highPing: [
+        "1510271113651818717",
+        "1510273045691109417"
+    ],
+
+    embedColors: {
+        LOW: 0x8EE89A,
+        MEDIUM: 0xD2E362,
+        HIGH: 0xE3A262,
+        CRITICAL: 0xE36262
+    }
+};
